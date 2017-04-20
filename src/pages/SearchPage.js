bbc2e4b9ed1076c4f02bbf51d2ea6c47c113c42a
zhuyucai <383326809@qq.com>
2017-04-20 21:13:37 +0800

    搜索页

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,94 @@
+/**
+ * @flow
+ */
+import React, { Component } from 'react'
+import {
+  View,
+} from 'react-native'
+import {
+  Container,
+  Header, InputGroup, Input, Icon, Button,
+  Text, Item, Content, List, ListItem,
+  Thumbnail, Body,
+} from 'native-base'
+import { connect } from 'react-redux'
+import { Actions } from 'react-native-router-flux'
+import { setSearchResultList } from "../actions/playerAction";
+
+class SearchPage extends Component {
+  constructor(){
+    super()
+    this.state ={
+      searchContent: '',
+    }
+  }
+  state: {
+    searchContent: string,
+  }
+  render() {
+    return(
+      <Container>
+        <Header searchBar rounded>
+          <Button
+            transparent
+            onPress={() =>{
+                    Actions.pop()
+                  } }
+          >
+            <Icon name='arrow-round-back' style={{ marginLeft: -10 }}/>
+          </Button>
+          <Item>
+            <Icon name="search" />
+            <Input
+              placeholder="搜索歌曲名或歌手名"
+              onChangeText={(v) => {
+                this.setState({
+                  searchContent: v,
+                })
+              }} />
+          </Item>
+          <Button
+            transparent
+            onPress={() => {
+              console.log('searchContent:', this.state.searchContent)
+              this.props.setSearchResultList(this.state.searchContent)
+            }}>
+            <Text>搜索</Text>
+          </Button>
+        </Header>
+        <Content>
+          <List dataArray={this.props.searchResultList || []}
+                renderRow={
+                  (item) =>
+                    <ListItem>
+                      <Thumbnail square size={80}
+                       source={{uri:item.f.split('|')[4]&&'http://imgcache.qq.com/music/photo/album_300/'+item.f.split('|')[4]%100+'/300_albumpic_'+item.f.split('|')[4]+'_0.jpg'}} />
+                      <Body>
+                      <Text>{item.fsong}</Text>
+                      <Text note>{item.fsinger}</Text>
+                      </Body>
+                    </ListItem>
+                  }>>
+
+          </List>
+        </Content>
+      </Container>
+    )
+  }
+}
+
+function mapProps(store) {
+  const { player } = store || {}
+  const { searchResultList } = player
+  return {
+    searchResultList,
+  }
+}
+
+function mapAction(dispatch) {
+  return {
+    setSearchResultList: async (r) => dispatch(await setSearchResultList(r)),
+  }
+}
+
+export default connect(mapProps, mapAction)(SearchPage)
