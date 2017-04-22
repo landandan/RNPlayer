ba9aca5f30767008116771a7bbb16a316f80ff93
zhuyucai <383326809@qq.com>
2017-04-22 11:30:03 +0800

    初始化数据

---
+++ src/utils/InitMusicList.js
@@ -0,0 +1,63 @@
+/**
+ * @flow
+ */
+export default {
+  "musicData": [
+    {
+      "id": 1,
+      "singer": "李玉刚",
+      "songName": "刚好遇见你",
+      "fileSrc": "http://m2.music.126.net/qv3RI4K7ABKJ0TyAdb3taw==/3250156397064860.mp3",
+      "imgSrc": "http://p4.music.126.net/lDyytkTaPYVTb1Vpide6AA==/18591642115187138.jpg"
+    },
+    {
+      "id": 2,
+      "singer": "以冬",
+      "songName": "我的一个道姑朋友",
+      "fileSrc": "http://m2.music.126.net/7WiRhPdirEJ2axW9Xm6uJQ==/1415071481819545.mp3",
+      "imgSrc": "http://omratag7g.bkt.clouddn.com/%E6%B0%91%E8%B0%A3.jpg"
+    },
+    {
+      "id": 3,
+      "singer": "任素汐",
+      "songName": "我要你",
+      "fileSrc": "http://m2.music.126.net/WhBIiLKYNa84TF3p5xx2FQ==/3265549607332004.mp3",
+      "imgSrc": "http://omratag7g.bkt.clouddn.com/%E6%88%91%E8%A6%81%E4%BD%A0.jpg"
+    },
+    {
+      "id": 4,
+      "singer": "赵雷",
+      "songName": "成都",
+      "fileSrc": "http://m2.music.126.net/7o5D4dA6271VktgawcbZFA==/18665309393829604.mp3",
+      "imgSrc": "http://p4.music.126.net/34YW1QtKxJ_3YnX9ZzKhzw==/2946691234868155.jpg"
+    },
+    {
+      "id": 5,
+      "singer": "韩安旭",
+      "songName": "多幸运",
+      "fileSrc": "http://m2.music.126.net/N_2fJbqpi8QDnV1YJPCsuw==/3424978721721344.mp3",
+      "imgSrc": "http://p3.music.126.net/U9RoJy0AUsoYuaKjmJbAzQ==/3386495814257685.jpg"
+    },
+    {
+      "id": 6,
+      "singer": "李健",
+      "songName": "假如爱有天意",
+      "fileSrc": "http://m2.music.126.net/sOdeUJ8DrQJpbJjjVogKuw==/7957165651997665.mp3",
+      "imgSrc": "http://omratag7g.bkt.clouddn.com/%E6%9D%8E%E5%81%A5.jpg"
+    },
+    {
+      "id": 7,
+      "singer": "Jam",
+      "songName": "七月上",
+      "fileSrc": "http://m2.music.126.net/K1SFXCvWf8BO9VEpSvx2ew==/7967061257205150.mp",
+      "imgSrc": "http://p3.music.126.net/9kZl6NRj3HxmQQ8DqTjZ4Q==/17729624997966923.jpg"
+    },
+    {
+      "id": 8,
+      "singer": "刘德华",
+      "songName": "咱们屯里人(粤语版)",
+      "fileSrc": "http://m2.music.126.net/XbGWv4O15b0c1-w4pCtwgA==/3300733908037356.mp3",
+      "imgSrc": "http://omratag7g.bkt.clouddn.com/%E5%A4%8F%E6%B4%9B%E7%89%B9%E7%83%A6%E6%81%BC.jpg"
+    }
+  ]
+}
