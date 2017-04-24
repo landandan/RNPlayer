d7d16c6e2fc376b3d937dfba7e14d3ff89666929
zhuyucai <383326809@qq.com>
2017-04-24 13:40:01 +0800

    删除无用代码，静音、音量调节更改为从store

--- src/component/player/VolumeModule.js
+++ src/component/player/VolumeModule.js
@@ -10,19 +10,21 @@ import {
 import {
   Icon,
 } from 'native-base'
+import { connect } from 'react-redux'
+import { mutedChange, volumeChange } from "../../actions/playerAction";
 
 class VolumeModule extends Component {
   constructor(props){
     super(props)
   }
   props: {
-    muted: boolean,
+    status: Object,
     mutedChange: () => void,
-    volume: number,
     volumeChange: (v) => void,
   }
 
   render() {
+    const { muted, volume } = this.props.status
     return (
       <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
         <TouchableOpacity
@@ -30,11 +32,11 @@ class VolumeModule extends Component {
           style={{ marginLeft: 10 }}
         >
           {/*volume-off:静音,volume-up：有声音*/}
-          <Icon name={this.props.muted? 'volume-off': 'volume-up'}/>
+          <Icon name={muted? 'volume-off': 'volume-up'}/>
         </TouchableOpacity>
         <Slider
           style={{ marginLeft: 10, marginRight: 10, flex: 1}}
-          value={this.props.volume}
+          value={volume}
           step={0.1}
           minimumTrackTintColor='#FFDB42'
           onValueChange={(value) => this.props.volumeChange(value)}
@@ -44,4 +46,19 @@ class VolumeModule extends Component {
   }
 }
 
-export default VolumeModule
+function mapProps(store) {
+  const { player } = store || {}
+  const { status = {} } = player
+  return {
+    status,
+  }
+}
+
+function mapAction(dispatch) {
+  return {
+    mutedChange: () => dispatch(mutedChange()),
+    volumeChange: (v) => dispatch(volumeChange(v)),
+  }
+}
+
+export default connect(mapProps, mapAction)(VolumeModule)
