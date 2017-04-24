5c2fc2c8525908512f20819949ab743559a00efd
zhuyucai <383326809@qq.com>
2017-04-24 14:35:13 +0800

    样式微调

--- src/component/player/Disc.js
+++ src/component/player/Disc.js
@@ -15,8 +15,9 @@ class Disc extends Component {
     super(props);
     this.spinValue = new Animated.Value(0)
   }
+
   //旋转动画
-  spin () {
+  spin() {
     this.spinValue.setValue(0)
     myAnimate = Animated.timing(
       this.spinValue,
@@ -32,19 +33,24 @@ class Disc extends Component {
   componentWillMount() {
     this.spin()   //   启动旋转
   }
+
   render() {
     const spin = this.spinValue.interpolate({
       inputRange: [0, 1],
       outputRange: ['0deg', '360deg']
     })
-    return(
-      <View style={{ marginTop: 30 }}>
-        <Image source={require('../../images/胶片盘.png')} style={{width:220,height:220,alignSelf:'center'}}/>
+    return (
+      <View style={{ marginVertical: 30 }}>
+        <Image
+          source={require('../../images/胶片盘.png')}
+          style={{width: 220, height: 220, alignSelf:'center'}}/>
 
         {/*旋转小图*/}
         <Animated.Image
-          ref = 'myAnimate'
-          style={{width:140,height:140,marginTop: -180,alignSelf:'center',borderRadius: 140*0.5,transform: [{rotate: spin}]}}
+          ref='myAnimate'
+          style={{
+            width: 140, height: 140, marginTop: -180,
+            alignSelf:'center',borderRadius: 140*0.5,transform: [{rotate: spin}]}}
           source={require('../../images/header.jpeg')}
         />
       </View>
