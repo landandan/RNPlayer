4ad83af4c24ee8d2ccf81c236b8842a98600dcee
zhuyucai <383326809@qq.com>
2017-04-24 13:06:56 +0800

    添加onProgress事件监听，使音乐能够循环，播放状态由传值改为从store取值

--- src/component/Player.js
+++ src/component/Player.js
@@ -5,34 +5,67 @@ import React, { Component } from 'react'
 import {
   View,
 } from 'react-native'
+import { connect } from 'react-redux'
 import Video from 'react-native-video'
+import { playMusicListNext } from "../actions/playerAction";
 
 class Player extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
+    this.state = {
+      duration: 0.0,
+    }
   }
 
   props: {
-    fileSrc: string,
-    muted: boolean,
-    volume: number,
-    paused: boolean,
+    status: Object,
+    currentMusicInfo: Object,
+    playMusicListNext: () => void,
+  }
+
+  onLoad(data) {
+    //console.log('data:', data)
+    this.setState({
+      duration: data.duration,
+    })
+  }
+
+  onProgress = (data) => {
+    let val = parseInt(data.currentTime)
+    if ( val >= this.state.duration ) {
+      this.props.playMusicListNext()
+    }
   }
 
   render() {
-    //console.log('this.props.fileSrc:', this.props.fileSrc)
-    return(
+    const { muted, paused, volume } = this.props.status
+    const { fileSrc = '' } = this.props.currentMusicInfo
+    return (
       <Video
-        source={{uri: (this.props.fileSrc || 'http://m2.music.126.net/7WiRhPdirEJ2axW9Xm6uJQ==/1415071481819545.mp3')}}
+        source={{uri: (fileSrc || 'http://m2.music.126.net/7WiRhPdirEJ2axW9Xm6uJQ==/1415071481819545.mp3')}}
         //ref='video'
-        muted={this.props.muted}
-        volume={this.props.volume}
-        paused={this.props.paused}
-        //onProgress={(e) => this.onProgress(e)}
-        //onLoad={(e) => this.onLoad(e)}
+        muted={muted}
+        volume={volume}
+        paused={paused}
+        onProgress={(e) => this.onProgress(e)}
+        onLoad={(e) => this.onLoad(e)}
       />
     )
   }
 }
+function mapProps(store) {
+  const { player } = store || {}
+  const { currentMusicInfo = {}, status = {} } = player
+  return {
+    currentMusicInfo,
+    status,
+  }
+}
 
-export default Player
+function mapAction(dispatch) {
+  return {
+    playMusicListNext: () => dispatch(playMusicListNext()),
+  }
+}
+
+export default connect(mapProps, mapAction)(Player)
