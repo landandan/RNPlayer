0a544e3abec4dab2b9169ceb6098bb56cd825f3f
zhuyucai <383326809@qq.com>
2017-04-25 18:40:22 +0800

    图片自适应

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -12,34 +12,39 @@ import {
   Button,
   Thumbnail,
 } from 'native-base'
-import { Actions } from 'react-native-router-flux'
 
 class Sidebar extends Component {
   render() {
-    return(
+    return (
       <View style={{ flex: 1, backgroundColor: 'white' }}>
         <Image
           source={require('../images/BK1024.png')}
-          style={{ width: 256, height: 160, justifyContent: 'center', alignItems: 'center' }}>
+          style={{width: undefined,  height: undefined, justifyContent: 'center', }}>
           <Thumbnail
-            style={{width: 80, height: 80, borderRadius: 40}}
-            source={require('../images/header.jpeg')} />
+            style={{width: 80, height: 80, borderRadius: 40, marginVertical: 30, marginLeft: 20}}
+            source={require('../images/header.jpeg')}/>
         </Image>
         <Button
           iconLeft light full
           onPress={() => {alert('提示', '该功能正在开发中')}}
           style={{ marginBottom: 5, justifyContent: 'flex-start' }}>
-          <Icon name='bicycle' />
+          <Icon name='bicycle'/>
           <Text>动态</Text>
         </Button>
 
-        <Button light full style={{ marginBottom: 5, justifyContent: 'flex-start'  }}>
-          <Icon name='star' />
+        <Button
+          light full
+          onPress={() => {alert('提示', '该功能正在开发中')}}
+          style={{ marginBottom: 5, justifyContent: 'flex-start'  }}>
+          <Icon name='star'/>
           <Text>收藏</Text>
         </Button>
 
-        <Button light full style={{ justifyContent: 'flex-start'  }}>
-          <Icon name='alarm' />
+        <Button
+          light full
+          onPress={() => {alert('提示', '该功能正在开发中')}}
+          style={{ justifyContent: 'flex-start'  }}>
+          <Icon name='alarm'/>
           <Text>历史</Text>
         </Button>
       </View>
