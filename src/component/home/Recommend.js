4dbe5511c1fcf6f13d3e6476fc8ff4e0d8d572f1
zhuyucai <383326809@qq.com>
2017-05-02 16:16:33 +0800

    推荐模块

---
+++ src/component/home/Recommend.js
@@ -0,0 +1,42 @@
+/**
+ * @flow
+ */
+import React, { Component } from 'react'
+import {
+  View,
+  Dimensions,
+} from 'react-native'
+import { connect } from 'react-redux'
+import HomeSwiper from '../HomeSwiper'
+import SongList from '../SongList'
+
+const { width } = Dimensions.get('window')
+
+class Recommend extends Component {
+  props: {
+    banners: Array<Object>,
+    hotspot: Array<Object>,
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{height: width * 200 / 540, width}}>
+          <HomeSwiper banners={this.props.banners}/>
+        </View>
+        <SongList songList={this.props.hotspot}/>
+      </View>
+    )
+  }
+}
+
+function mapProps(store) {
+  const { NETSHomeData } = store.homePage || {}
+  const { banners = [], hotspot = [] } = NETSHomeData || {}
+  return {
+    banners,
+    hotspot,
+  }
+}
+
+export default connect(mapProps)(Recommend)
