176fa01dcadee6c504d40961ab75decbe0f7ad84
zhuyucai <383326809@qq.com>
2017-05-02 12:38:39 +0800

    API提取分类

--- src/actions/homeAction.js
+++ src/actions/homeAction.js
@@ -1,14 +1,14 @@
 /**
  * @flow
  */
-import { getNTESHomeMusic } from "../utils/API"
+import getMacNeteaseCloudHome from '../utils/API/NeteaseCloudMusicApi/macNeteaseCloudHome'
 
 export async function setNETSHomeData() {
-  const NETSHomeData = await getNTESHomeMusic()
+  const NETSHomeData = await getMacNeteaseCloudHome()
+  //console.log('NETSHomeData:', NETSHomeData)
   const hotspot = NETSHomeData['/api/discovery/hotspot'].data
   const newSongs = NETSHomeData['/api/v1/discovery/new/songs'].data
   const banners = NETSHomeData['/api/v2/banner/get'].banners
-  // console.log('NETSHomeData:', NETSHomeData)
   // console.log('hotspot:', hotspot)
   // console.log('newSongs:', newSongs)
   // console.log('banner:', banners)
