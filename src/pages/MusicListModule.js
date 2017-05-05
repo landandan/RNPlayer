d5ff979c7a90c4869a77ed13af63b4d85df3ef4c
zhuyucai <383326809@qq.com>
2017-05-05 11:01:40 +0800

    首页歌单列表模块

---
+++ src/pages/MusicListModule.js
@@ -0,0 +1,20 @@
+/**
+ * @flow
+ */
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+} from 'react-native'
+
+class MusicListModule extends Component {
+  render() {
+    return(
+      <View>
+        <Text>123</Text>
+      </View>
+    )
+  }
+}
+
+export default MusicListModule
