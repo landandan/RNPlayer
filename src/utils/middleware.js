08004866b4731600350d9b702c5ba7bdc4f66116
zhuyucai <383326809@qq.com>
2017-05-12 09:42:06 +0800

    middleware

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,12 +1,43 @@
 /**
  * @flow
  */
+import { startLoading, finishLoading } from "../actions/loading"
 
 export function multiDispatcher({ dispatch }) {
   return next => actions => {
-    if (Array.isArray(actions)) {
+    if ( Array.isArray(actions) ) {
       return actions.map(action => dispatch(action))
     }
     return next(actions)
+  }
+}
+
+export function promise({ dispatch }) {
+  return next => action => {
+    if ( action && typeof action.then === 'function' ) {
+      dispatch(startLoading())
+      const finishLoadingAndDispatch = (input) => {
+        dispatch(finishLoading())
+        dispatch(input)
+      }
+      return action.then(finishLoadingAndDispatch).catch(finishLoadingAndDispatch)
+    }
+    return next(action)
+  }
+}
+
+export function thunkState({ dispatch, getState }) {
+  return next => action => {
+    try {
+      if ( getState().loading.loadingQueue.length <= 0 ) {
+        dispatch(finishLoading())
+      }
+    } catch (e) {
+      dispatch(finishLoading())
+    }
+    if ( action && typeof action === 'function' ) {
+      return dispatch(action(getState()))
+    }
+    return next(action)
   }
 }
