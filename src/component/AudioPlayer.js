68c4a31f9ca083856539cdd49568986072f68c7d
zhuyucai <383326809@qq.com>
2017-05-15 10:02:20 +0800

    添加错误提示

--- src/component/AudioPlayer.js
+++ src/component/AudioPlayer.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native'
 import { connect } from 'react-redux'
 import Video from 'react-native-video'
+import { Toast } from 'native-base'
 import { playMusicListNext, setCurrentMusicDuration, setMusicCurrentTime } from "../actions/playerAction";
 
 class AudioPlayer extends Component {
@@ -34,6 +35,12 @@ class AudioPlayer extends Component {
   onError(data) {
     //console.log('onError:', data)
     if(data.error){
+      Toast.show({
+        text: '无法连接服务器,自动播放下一首',
+        position: 'bottom',
+        duration: 3000,
+        type: 'warning',
+      })
       this.props.playMusicListNext()
     }
   }
