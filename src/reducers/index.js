bbe5c248ec9ed3c6eaf52ead7c91fb21adf467c3
zhuyucai <383326809@qq.com>
2017-05-15 10:04:55 +0800

    新增reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,16 @@ import { combineReducers } from 'redux'
 import player from './playerReducer'
 import homePage from './homeReducer'
 import joke from './JokeReducer'
-import route from './route'
+import loading from "./loadingReducer"
+import searchPage from './searchReducer'
+import route from './routeReducer'
 
 const reducers = {
-  player,
+  loading,
   homePage,
+  player,
   joke,
   //route,
+  searchPage,
 }
 export default combineReducers(reducers)
