73c988804f24ae65580f90d78278b07487d8fca8
zhuyucai <383326809@qq.com>
2017-07-03 15:39:10 +0800

    优化，由于网易云数据是动态的，所以每次播放前查询一遍数据源

--- src/component/AudioPlayer.js
+++ src/component/AudioPlayer.js
@@ -5,22 +5,42 @@ import React, { Component } from 'react'
 import {
   View,
 } from 'react-native'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 import Video from 'react-native-video'
 import { Toast } from 'native-base'
-import { playMusicListNext, setCurrentMusicDuration, setMusicCurrentTime } from '../actions/playerAction'
+import {
+  playMusicListNext,
+  setCurrentMusicDuration,
+  setMusicCurrentTime
+} from '../actions/playerAction'
+import { getMusicUrlById } from "../utils/API/NeteaseCloudMusicApi/fetchNeteaseNode"
 
 class AudioPlayer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      url: '',
+    }
   }
 
   props: {
     status: Object,
     currentMusicInfo: Object,
     playMusicListNext: () => void,
-    setCurrentMusicDuration: (v) => void,
-    setMusicCurrentTime: (v) => void,
+    setCurrentMusicDuration: () => void,
+    setMusicCurrentTime: () => void,
+  }
+
+  async componentWillReceiveProps(nextProps: Object) {
+    if (nextProps.currentMusicInfo.id != this.props.currentMusicInfo.id) {
+      const { id = '' } = nextProps.currentMusicInfo
+      const musicUrlResult = await getMusicUrlById(id)
+      const musicUrl = _.find(musicUrlResult.data, { id, })
+      this.setState({
+        url: musicUrl.url
+      })
+    }
   }
 
   onLoad(data) {
@@ -32,24 +52,30 @@ class AudioPlayer extends Component {
     // console.log('onEnd:', data)
     this.props.playMusicListNext()
   }
+
   onError(data) {
     // console.log('onError:', data)
-    if (data.error) {
+    if ( data.error ) {
       Toast.show({
         text: '无法连接服务器,自动播放下一首',
         position: 'bottom',
         duration: 3000,
         type: 'warning',
       })
-      this.props.playMusicListNext()
+      if ( !this.props.status.paused ) {
+        this.props.playMusicListNext()
+      }
     }
   }
+
   onBuffer(data) {
     console.log('onBuffer:', data)
   }
+
   onLoadStart(data) {
     console.log('onLoadStart:', data)
   }
+
   onTimedMetadata(data) {
     console.log('onTimedMetadata:', data)
   }
@@ -61,21 +87,24 @@ class AudioPlayer extends Component {
 
   render() {
     const { muted, paused, volume, cyclicalPattern } = this.props.status
-    const { mp3Url = '' } = this.props.currentMusicInfo
     return (
-      <Video
-        source={{ uri: mp3Url }}
-        // ref='video'
-        muted={muted}
-        volume={volume}
-        paused={paused}
-        repeat={cyclicalPattern == 'repeat'}
-        onProgress={e => this.onProgress(e)}
-        onLoad={e => this.onLoad(e)}
-        onEnd={e => this.onEnd(e)}
-        onError={e => this.onError(e)}
-        playInBackground
-      />
+      <View>
+        {
+          !_.isEmpty(this.state.url) && <Video
+            source={{ uri: this.state.url }}
+            // ref='video'
+            muted={muted}
+            volume={volume}
+            paused={paused}
+            repeat={cyclicalPattern == 'repeat'}
+            onProgress={e => this.onProgress(e)}
+            onLoad={e => this.onLoad(e)}
+            onEnd={e => this.onEnd(e)}
+            onError={e => this.onError(e)}
+            playInBackground
+          />
+        }
+      </View>
     )
   }
 }
